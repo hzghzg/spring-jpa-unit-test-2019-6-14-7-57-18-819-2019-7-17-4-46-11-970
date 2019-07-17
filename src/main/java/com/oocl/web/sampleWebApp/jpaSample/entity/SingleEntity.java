bf80564e0ba5d205f948569db61908db6428be00
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-17 18:05:35 +0800

    feat:add getName method to pass test name test_should_return_user_when_the_user_exist

--- src/main/java/com/oocl/web/sampleWebApp/jpaSample/entity/SingleEntity.java
+++ src/main/java/com/oocl/web/sampleWebApp/jpaSample/entity/SingleEntity.java
@@ -1,9 +1,11 @@
 package com.oocl.web.sampleWebApp.jpaSample.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class SingleEntity {
+    @Id
     private Long id;
     private String name;
     public SingleEntity() {
@@ -15,6 +17,22 @@ public class SingleEntity {
 
     public SingleEntity(Long id, String name) {
         this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 }
